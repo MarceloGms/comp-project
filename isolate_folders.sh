691000628c6d74b2a5386023cc32eea76df35818
MarceloGms <126262388+MarceloGms@users.noreply.github.com>
2023-12-17 23:31:16 +0000

    Delete isolate_folders.sh

--- isolate_folders.sh
+++
@@ -1,9 +0,0 @@
-meta="meta3"
-
-for file in "./c/$meta"/*c; do
-    file_name=$(basename "${file%.*}")
-    echo "Testing ${file%.*}"
-    echo "filaname $file_name"
-    mkdir ./c/$meta/$file_name
-    mv "${file%.*}".* ./c/$meta/$file_name/
-done
